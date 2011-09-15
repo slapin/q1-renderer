7634a6cf11a6d48c4af9da05afed0584a683f2c8
Sergey Lapin <slapin@ossfans.org>
2011-09-15 19:54:25 +0400

    Added all functions for PolysetDraw

--- poly.c
+++ poly.c
@@ -467,3 +467,370 @@ Referenced by D_DrawNonSubdiv().
     }
 }
 
+void D_PolysetRecursiveTriangle(int *p1, int *p2, int *p3)
+/*
+Definition at line 334 of file d_polyse.c.
+
+References d_pcolormap, d_scantable, d_viewbuffer, skintable, and zspantable.
+
+Referenced by D_DrawSubdiv().
+*/
+{
+    int     *temp;
+    int     d;
+    int     new[6];
+    int     z;
+    short   *zbuf;
+
+    d = lp2[0] - lp1[0];
+    if (d < -1 || d > 1)
+        goto split;
+    d = lp2[1] - lp1[1];
+    if (d < -1 || d > 1)
+        goto split;
+
+    d = lp3[0] - lp2[0];
+    if (d < -1 || d > 1)
+        goto split2;
+    d = lp3[1] - lp2[1];
+    if (d < -1 || d > 1)
+        goto split2;
+
+    d = lp1[0] - lp3[0];
+    if (d < -1 || d > 1)
+        goto split3;
+    d = lp1[1] - lp3[1];
+    if (d < -1 || d > 1)
+    {
+split3:
+        temp = lp1;
+        lp1 = lp3;
+        lp3 = lp2;
+        lp2 = temp;
+
+        goto split;
+    }
+
+    return;         // entire tri is filled
+
+split2:
+    temp = lp1;
+    lp1 = lp2;
+    lp2 = lp3;
+    lp3 = temp;
+
+split:
+// split this edge
+    new[0] = (lp1[0] + lp2[0]) >> 1;
+    new[1] = (lp1[1] + lp2[1]) >> 1;
+    new[2] = (lp1[2] + lp2[2]) >> 1;
+    new[3] = (lp1[3] + lp2[3]) >> 1;
+    new[5] = (lp1[5] + lp2[5]) >> 1;
+
+// draw the point if splitting a leading edge
+    if (lp2[1] > lp1[1])
+        goto nodraw;
+    if ((lp2[1] == lp1[1]) && (lp2[0] < lp1[0]))
+        goto nodraw;
+
+
+    z = new[5]>>16;
+    zbuf = zspantable[new[1]] + new[0];
+    if (z >= *zbuf)
+    {
+        int     pix;
+        
+        *zbuf = z;
+        pix = d_pcolormap[skintable[new[3]>>16][new[2]>>16]];
+        d_viewbuffer[d_scantable[new[1]] + new[0]] = pix;
+    }
+
+nodraw:
+// recursively continue
+    D_PolysetRecursiveTriangle (lp3, lp1, new);
+    D_PolysetRecursiveTriangle (lp3, new, lp2);
+}
+
+
+void D_PolysetCalcGradients(int skinwidth)
+/*
+Definition at line 553 of file d_polyse.c.
+
+References a_sstepxfrac, a_ststepxwhole, a_tstepxfrac, d_xdenom, int, r_lstepx, r_lstepy, r_p0, r_p1, r_p2, r_sstepx, r_sstepy, r_tstepx, r_tstepy, r_zistepx, and r_zistepy.
+
+Referenced by D_RasterizeAliasPolySmooth().
+
+*/
+{
+    float   xstepdenominv, ystepdenominv, t0, t1;
+    float   p01_minus_p21, p11_minus_p21, p00_minus_p20, p10_minus_p20;
+
+    p00_minus_p20 = r_p0[0] - r_p2[0];
+    p01_minus_p21 = r_p0[1] - r_p2[1];
+    p10_minus_p20 = r_p1[0] - r_p2[0];
+    p11_minus_p21 = r_p1[1] - r_p2[1];
+
+    xstepdenominv = 1.0 / (float)d_xdenom;
+
+    ystepdenominv = -xstepdenominv;
+
+// ceil () for light so positive steps are exaggerated, negative steps
+// diminished,  pushing us away from underflow toward overflow. Underflow is
+// very visible, overflow is very unlikely, because of ambient lighting
+    t0 = r_p0[4] - r_p2[4];
+    t1 = r_p1[4] - r_p2[4];
+    r_lstepx = (int)
+            ceil((t1 * p01_minus_p21 - t0 * p11_minus_p21) * xstepdenominv);
+    r_lstepy = (int)
+            ceil((t1 * p00_minus_p20 - t0 * p10_minus_p20) * ystepdenominv);
+
+    t0 = r_p0[2] - r_p2[2];
+    t1 = r_p1[2] - r_p2[2];
+    r_sstepx = (int)((t1 * p01_minus_p21 - t0 * p11_minus_p21) *
+            xstepdenominv);
+    r_sstepy = (int)((t1 * p00_minus_p20 - t0* p10_minus_p20) *
+            ystepdenominv);
+
+    t0 = r_p0[3] - r_p2[3];
+    t1 = r_p1[3] - r_p2[3];
+    r_tstepx = (int)((t1 * p01_minus_p21 - t0 * p11_minus_p21) *
+            xstepdenominv);
+    r_tstepy = (int)((t1 * p00_minus_p20 - t0 * p10_minus_p20) *
+            ystepdenominv);
+
+    t0 = r_p0[5] - r_p2[5];
+    t1 = r_p1[5] - r_p2[5];
+    r_zistepx = (int)((t1 * p01_minus_p21 - t0 * p11_minus_p21) *
+            xstepdenominv);
+    r_zistepy = (int)((t1 * p00_minus_p20 - t0 * p10_minus_p20) *
+            ystepdenominv);
+
+#if id386
+    a_sstepxfrac = r_sstepx << 16;
+    a_tstepxfrac = r_tstepx << 16;
+#else
+    a_sstepxfrac = r_sstepx & 0xFFFF;
+    a_tstepxfrac = r_tstepx & 0xFFFF;
+#endif
+
+    a_ststepxwhole = skinwidth * (r_tstepx >> 16) + (r_sstepx >> 16);
+}
+void D_PolysetDrawSpans8(spanpackage_t *pspanpackage)
+/*
+Definition at line 634 of file d_polyse.c.
+
+References a_sstepxfrac, a_ststepxwhole, a_tstepxfrac, acolormap, spanpackage_t::count, d_aspancount, d_countextrastep, erroradjustdown, erroradjustup, errorterm, spanpackage_t::light, spanpackage_t::pdest, spanpackage_t::ptex, spanpackage_t::pz, r_affinetridesc, r_lstepx, r_zistepx, spanpackage_t::sfrac, affinetridesc_t::skinwidth, spanpackage_t::tfrac, ubasestep, and spanpackage_t::zi.
+
+Referenced by D_RasterizeAliasPolySmooth().
+*/
+{
+    int     lcount;
+    byte    *lpdest;
+    byte    *lptex;
+    int     lsfrac, ltfrac;
+    int     llight;
+    int     lzi;
+    short   *lpz;
+
+    do
+    {
+        lcount = d_aspancount - pspanpackage->count;
+
+        errorterm += erroradjustup;
+        if (errorterm >= 0)
+        {
+            d_aspancount += d_countextrastep;
+            errorterm -= erroradjustdown;
+        }
+        else
+        {
+            d_aspancount += ubasestep;
+        }
+
+        if (lcount)
+        {
+            lpdest = pspanpackage->pdest;
+            lptex = pspanpackage->ptex;
+            lpz = pspanpackage->pz;
+            lsfrac = pspanpackage->sfrac;
+            ltfrac = pspanpackage->tfrac;
+            llight = pspanpackage->light;
+            lzi = pspanpackage->zi;
+
+            do
+            {
+                if ((lzi >> 16) >= *lpz)
+                {
+                    *lpdest = ((byte *)acolormap)[*lptex + (llight & 0xFF00)];
+// gel mapping                  *lpdest = gelmap[*lpdest];
+                    *lpz = lzi >> 16;
+                }
+                lpdest++;
+                lzi += r_zistepx;
+                lpz++;
+                llight += r_lstepx;
+                lptex += a_ststepxwhole;
+                lsfrac += a_sstepxfrac;
+                lptex += lsfrac >> 16;
+                lsfrac &= 0xFFFF;
+                ltfrac += a_tstepxfrac;
+                if (ltfrac & 0x10000)
+                {
+                    lptex += r_affinetridesc.skinwidth;
+                    ltfrac &= 0xFFFF;
+                }
+            } while (--lcount);
+        }
+
+        pspanpackage++;
+    } while (pspanpackage->count != -999999);
+}
+
+void D_PolysetScanLeftEdge(int height)
+/*
+Definition at line 443 of file d_polyse.c.
+
+References spanpackage_t::count, d_aspancount, d_countextrastep, d_light, d_lightbasestep, d_lightextrastep, d_pdest, d_pdestbasestep, d_pdestextrastep, d_ptex, d_ptexbasestep, d_ptexextrastep, d_pz, d_pzbasestep, d_pzextrastep, d_sfrac, d_sfracbasestep, d_sfracextrastep, d_tfrac, d_tfracbasestep, d_tfracextrastep, d_zi, d_zibasestep, d_ziextrastep, erroradjustdown, erroradjustup, errorterm, spanpackage_t::light, spanpackage_t::pdest, spanpackage_t::ptex, spanpackage_t::pz, r_affinetridesc, spanpackage_t::sfrac, affinetridesc_t::skinwidth, spanpackage_t::tfrac, ubasestep, and spanpackage_t::zi.
+
+Referenced by D_RasterizeAliasPolySmooth().
+*/
+{
+
+    do
+    {
+        d_pedgespanpackage->pdest = d_pdest;
+        d_pedgespanpackage->pz = d_pz;
+        d_pedgespanpackage->count = d_aspancount;
+        d_pedgespanpackage->ptex = d_ptex;
+
+        d_pedgespanpackage->sfrac = d_sfrac;
+        d_pedgespanpackage->tfrac = d_tfrac;
+
+    // FIXME: need to clamp l, s, t, at both ends?
+        d_pedgespanpackage->light = d_light;
+        d_pedgespanpackage->zi = d_zi;
+
+        d_pedgespanpackage++;
+
+        errorterm += erroradjustup;
+        if (errorterm >= 0)
+        {
+            d_pdest += d_pdestextrastep;
+            d_pz += d_pzextrastep;
+            d_aspancount += d_countextrastep;
+            d_ptex += d_ptexextrastep;
+            d_sfrac += d_sfracextrastep;
+            d_ptex += d_sfrac >> 16;
+
+            d_sfrac &= 0xFFFF;
+            d_tfrac += d_tfracextrastep;
+            if (d_tfrac & 0x10000)
+            {
+                d_ptex += r_affinetridesc.skinwidth;
+                d_tfrac &= 0xFFFF;
+            }
+            d_light += d_lightextrastep;
+            d_zi += d_ziextrastep;
+            errorterm -= erroradjustdown;
+        }
+        else
+        {
+            d_pdest += d_pdestbasestep;
+            d_pz += d_pzbasestep;
+            d_aspancount += ubasestep;
+            d_ptex += d_ptexbasestep;
+            d_sfrac += d_sfracbasestep;
+            d_ptex += d_sfrac >> 16;
+            d_sfrac &= 0xFFFF;
+            d_tfrac += d_tfracbasestep;
+            if (d_tfrac & 0x10000)
+            {
+                d_ptex += r_affinetridesc.skinwidth;
+                d_tfrac &= 0xFFFF;
+            }
+            d_light += d_lightbasestep;
+            d_zi += d_zibasestep;
+        }
+    } while (--height);
+}
+
+void D_PolysetSetUpForLineScan(fixed8_t startvertu, fixed8_t startvertv, fixed8_t endvertu, fixed8_t endvertv)
+/*
+Definition at line 512 of file d_polyse.c.
+
+References erroradjustdown, erroradjustup, errorterm, FloorDivMod(), adivtab_t::quotient, adivtab_t::remainder, and ubasestep.
+
+Referenced by D_RasterizeAliasPolySmooth().
+*/
+{
+    double      dm, dn;
+    int         tm, tn;
+    adivtab_t   *ptemp;
+
+// TODO: implement x86 version
+
+    errorterm = -1;
+
+    tm = endvertu - startvertu;
+    tn = endvertv - startvertv;
+
+    if (((tm <= 16) && (tm >= -15)) &&
+        ((tn <= 16) && (tn >= -15)))
+    {
+        ptemp = &adivtab[((tm+15) << 5) + (tn+15)];
+        ubasestep = ptemp->quotient;
+        erroradjustup = ptemp->remainder;
+        erroradjustdown = tn;
+    }
+    else
+    {
+        dm = (double)tm;
+        dn = (double)tn;
+
+        FloorDivMod (dm, dn, &ubasestep, &erroradjustup);
+
+        erroradjustdown = dn;
+    }
+}
+#define PARANOID
+void FloorDivMod(double number, double denom, int *quotient, int *rem)
+/*
+Definition at line 297 of file mathlib.c.
+
+References int, and Sys_Error().
+
+Referenced by D_PolysetSetUpForLineScan().
+*/
+{
+    int q, r;
+    double x;
+
+#ifndef PARANOID
+    if (denom <= 0.0)
+        Sys_Error ("FloorDivMod: bad denominator %d", denom);
+#endif
+
+    if (numer >= 0.0) {
+        x = floor(numer / denom);
+        q = (int) x;
+        r = (int) floor(numer - (x * denom));
+    } else {
+        // perform operations with positive values, and fix mod to make floor-based
+        x = floor(-numer / denom);
+        q = -(int)x;
+        r = (int)floor(-numer - (x * denom));
+        if (r != 0) {
+            q--;
+            r = (int)denom - r;
+        }
+    }
+    *quotient = q;
+    *rem = r;
+}
+
+int main()
+{
+	D_PolysetDraw();
+	return 0;
+}
+
