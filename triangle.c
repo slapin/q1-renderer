5081136fd88a7aad2143bea14b125f25292d10d7
Sergey Lapin <slapin@ossfans.org>
2011-11-06 04:07:58 +0400

    Removed recursion from triangles build

--- triangle.c
+++ triangle.c
@@ -1,24 +1,58 @@
 #include "triangle.h"
-void D_PolysetRecursiveTriangle(int *lp1, int *lp2, int *lp3,
+static int triangle_core(int *lp1, int *lp2, int *lp3);
+
+static void triangle_split(int *lp1, int *lp2, int *lp3,
 				       unsigned char *d_pcolormap,
 				       unsigned char *d_viewbuffer,
-				       const short **zspantable,
+				       short * const *zspantable,
 				       unsigned char **skintable,
-				       const int *d_scantable)
-/*
-Definition at line 334 of file d_polyse.c.
-
-References d_pcolormap, d_scantable, d_viewbuffer, skintable, and zspantable.
-
-Referenced by D_DrawSubdiv().
-*/
+				       const int *d_scantable, int ld)
 {
-	int *temp;
-	int d;
 	int new[6];
 	int z;
 	short *zbuf;
+	int *temp;
+// split this edge
+	while(1) {
+		temp = lp1;
+		lp1 = lp3;
+		if (ld) {
+			lp2 = temp;
+			lp3 = new;
+		} else {
+			temp = lp2;
+			lp2 = new;
+			lp3 = temp;
+		}
+		new[0] = (lp1[0] + lp2[0]) >> 1;
+		new[1] = (lp1[1] + lp2[1]) >> 1;
+		new[2] = (lp1[2] + lp2[2]) >> 1;
+		new[3] = (lp1[3] + lp2[3]) >> 1;
+		new[5] = (lp1[5] + lp2[5]) >> 1;
+		if (!triangle_core(lp1, lp2, lp3))
+			break;
+		if (lp2[1] > lp1[1])
+			goto nodraw;
+		if ((lp2[1] == lp1[1]) && (lp2[0] < lp1[0]))
+			goto nodraw;
 
+		z = new[5] >> 16;
+		zbuf = zspantable[new[1]] + new[0];
+		if (z >= *zbuf) {
+			int pix;
+
+			*zbuf = z;
+			pix = d_pcolormap[skintable[new[3] >> 16][new[2] >> 16]];
+			d_viewbuffer[d_scantable[new[1]] + new[0]] = pix;
+		}
+nodraw:
+		ld ^= ld;
+	}
+}
+static int triangle_core(int *lp1, int *lp2, int *lp3)
+{
+	int d;
+	int *temp;
 	d = lp2[0] - lp1[0];
 	if (d < -1 || d > 1)
 		goto split;
@@ -47,7 +81,7 @@ Referenced by D_DrawSubdiv().
 		goto split;
 	}
 
-	return;			// entire tri is filled
+	return 0;			// entire tri is filled
 
  split2:
 	temp = lp1;
@@ -56,44 +90,24 @@ Referenced by D_DrawSubdiv().
 	lp3 = temp;
 
  split:
-// split this edge
-#if 0
-	printf("%s:%d lp1[1] + lp2[1] = %d\n", __func__, __LINE__,
-	       lp1[1] + lp2[1]);
-#endif
-	new[0] = (lp1[0] + lp2[0]) >> 1;
-	new[1] = (lp1[1] + lp2[1]) >> 1;
-	new[2] = (lp1[2] + lp2[2]) >> 1;
-	new[3] = (lp1[3] + lp2[3]) >> 1;
-	new[5] = (lp1[5] + lp2[5]) >> 1;
+	return 1;
+}
+void D_PolysetRecursiveTriangle(int *lp1, int *lp2, int *lp3,
+				       unsigned char *d_pcolormap,
+				       unsigned char *d_viewbuffer,
+				       short * const *zspantable,
+				       unsigned char **skintable,
+				       const int *d_scantable)
+/*
+Definition at line 334 of file d_polyse.c.
 
-// draw the point if splitting a leading edge
-	if (lp2[1] > lp1[1])
-		goto nodraw;
-	if ((lp2[1] == lp1[1]) && (lp2[0] < lp1[0]))
-		goto nodraw;
+References d_pcolormap, d_scantable, d_viewbuffer, skintable, and zspantable.
 
-	z = new[5] >> 16;
-	zbuf = zspantable[new[1]] + new[0];
-	if (z >= *zbuf) {
-		int pix;
+Referenced by D_DrawSubdiv().
+*/
+{
 
-		*zbuf = z;
-		pix = d_pcolormap[skintable[new[3] >> 16][new[2] >> 16]];
-#if 0
-		printf("%p = %d\n", &d_viewbuffer[d_scantable[new[1]] + new[0]],
-		       pix);
-#endif
-		d_viewbuffer[d_scantable[new[1]] + new[0]] = pix;
-#if 0
-		printf("plot %d %d %d = %d\n", new[1], d_scantable[new[1]],
-		       new[0], pix);
-#endif
-	}
-
- nodraw:
-// recursively continue
-	D_PolysetRecursiveTriangle(lp3, lp1, new, d_pcolormap, d_viewbuffer, zspantable, skintable, d_scantable);
-	D_PolysetRecursiveTriangle(lp3, new, lp2, d_pcolormap, d_viewbuffer, zspantable, skintable, d_scantable);
+	triangle_split(lp1, lp2, lp3, d_pcolormap, d_viewbuffer,
+			zspantable, skintable, d_scantable, 0);
 }
 
--- triangle.h
+++ triangle.h
@@ -1,6 +1,6 @@
 void D_PolysetRecursiveTriangle(int *lp1, int *lp2, int *lp3,
 				       unsigned char *d_pcolormap,
 				       unsigned char *d_viewbuffer,
-				       const short **zspantable,
+				       short * const *zspantable,
 				       unsigned char **skintable,
 				       const int *d_scantable);
